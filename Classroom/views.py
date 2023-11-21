c26648ac18f189700f5e310814f65179cf950837
Minh Tiến Lê <minhtien.etbk@gmail.com>
2023-11-21 20:53:34 +0700

    Start-11/21/23

---
+++ Classroom/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+# from django.http import HttpResponseRedirect
+
+# Create your views here.
+
+
+def classroom(request):
+
+    template = loader.get_template('base.html')
+    # context = {
+    #     'myStudents' : mystudents,
+    # }
+    return HttpResponse(template.render())
