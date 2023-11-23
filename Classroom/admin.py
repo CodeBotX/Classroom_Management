4864c7f438066315c7e2482f017d2990358a1649
Minh Tiến Lê <minhtien.etbk@gmail.com>
2023-11-24 00:16:11 +0700

    add templates, models 21/11

--- Classroom/admin.py
+++ Classroom/admin.py
@@ -1,11 +1,7 @@
+# Dùng để hiển thị các model trong giao diện quản trị Dj. 
+
 from django.contrib import admin
-from .models import Member
+
+
 # Register your models here.
 
-
-
-class MemberAdmin(admin.ModelAdmin):
-  list_display = ("firstname", "lastname", "joined_date")
-  list_filter = ['firstname']
-  
-admin.site.register(Member, MemberAdmin)
--- Classroom/models.py
+++ Classroom/models.py
@@ -1,17 +1,7 @@
 from django.db import models
-
+from django.core.exceptions import ValidationError
 # Create your models here.
 
-# ---------- Class Member khởi tạo các thành viên quản lý ----------
-class Member(models.Model):
-  firstname = models.CharField(max_length=255)
-  lastname = models.CharField(max_length=255)
-  joined_date = models.DateField(null=True)
-
-  # def __str__(self):
-  #   return f"{self.firstname} {self.lastname}"
-
-# -------------------------------------------------------------------
 
 # ---------- Class Subject khởi tạo đối tượng môn học ----------
 class Subject (models.Model):
@@ -22,11 +12,41 @@ class Subject (models.Model):
 # ---------- Class Person là cha của teacher và student ----------
 class Person:
   def __init__(self, name):
-    self.name = name
+    self.name = models.CharField(max_length=50)
   
 # ----------------------------------------------------------------
 # ---------- Class teacher ----------
 class Teacher (Person):
   def __init__(self, name,subject):
     super().__init__(name)
-    self.subject = subject
+    self.subject = models.CharField(max_length=20)
+  def grade_student (self,student ,score):
+    # cho điểm học sinh
+    pass
+    
+class Student(Person):
+  comment = models.TextField()
+  def __init__(self, name,id):
+    super().__init__(name)
+    self.id = models.ImageField(primary_key=True)
+  def save(self, *args, **kwargs):
+    if not 1000<= self.id <10000:
+      raise
+    ValidationError("ID Là 4 Số")
+    super().save(*args, **kwargs)
+    
+class Schedule(models.Model):
+    DAYS_OF_WEEK = [
+        ('MON', 'Monday'),
+        ('TUE', 'Tuesday'),
+        ('WED', 'Wednesday'),
+        ('THU', 'Thursday'),
+        ('FRI', 'Friday'),
+        ('SAT', 'Saturday'),
+        ('SUN', 'Sunday'),
+    ]
+
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    day_of_week = models.CharField(max_length=3, choices=DAYS_OF_WEEK)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
