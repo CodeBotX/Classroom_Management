82ba09f304c3ea5e370cfc06f9879f3fd7ec01d7
Minh Tiến Lê <minhtien.etbk@gmail.com>
2023-12-17 22:56:45 +0700

    minhtien add login

--- Classroom/urls.py
+++ Classroom/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'),name='login'),
-    path('classroom/', views.classroom, name='Classroom'),
-    path('home/', views.Home, name='home'),
-    path('register/', views.register, name='register'),
-    path('logout', auth_views.LogoutView.as_view, {'next_page':'login'}, name="logout"),
+    # path('login/', auth_views.LoginView.as_view(template_name='login.html'),name='login'),
+    path('login/', views.siteLoginView.as_view(),name='login'),
+    path('classroom/',views.classroom_view , name='Classroom'),
+    path('home/', views.Home_view, name='home'),
+    path('register/', views.register_view, name='register'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name="logout"),
 ]
+
+# {'next_page':'login'}
+# login_required(views.classroom_view)
--- Classroom/views.py
+++ Classroom/views.py
@@ -4,14 +4,16 @@ from django.template import loader
 from .models import *
 from .forms import RegistrationForm
 from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
 
 
 # from django.http import HttpResponseRedirect
 
 # Create your views here.
 
-
-def classroom(request):
+@login_required(login_url='/login/')
+def classroom_view(request):
     mystudents = Student.objects.all()
     template = loader.get_template('classroom.html')
     context = {
@@ -20,7 +22,7 @@ def classroom(request):
     return HttpResponse(template.render(context,request))
 
 
-def register (request):
+def register_view (request):
   form = RegistrationForm()
   template = loader.get_template('register.html')
   if request.method == 'POST':
@@ -34,7 +36,11 @@ def register (request):
   return HttpResponse(template.render(context,request))
 
 
-def Home(request):
+def Home_view(request):
     template = loader.get_template('home.html')
     return HttpResponse(template.render())
-  
+
+
+class siteLoginView ( TemplateView ):
+  #  tạm thời để login 2
+  template_name = 'login2.html' 
