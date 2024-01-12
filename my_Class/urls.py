4b4059af9184fbff2eeacf3f72a50bc7338ae763
Minh Tiến Lê <minhtien.etbk@gmail.com>
2024-01-12 18:11:33 +0700

    add view option

--- my_Class/urls.py
+++ my_Class/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     # path('login/', auth_views.LoginView.as_view(template_name='login.html'),name='login'),
     # path('login/', views.siteLoginView.as_view(),name='login'),
     path('home/', CustomLoginView.as_view(template_name='login.html'), name='home'),
-    path('option/', views.option_view, name='register'),
+    path('option/', views.option_view, name='option'),
     path('classroom/',views.classroom_view , name='Classroom'),
     path('register/', views.register_view, name='register'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name="logout"),
--- my_Class/views.py
+++ my_Class/views.py
@@ -5,11 +5,11 @@ from .models import *
 # from .forms import RegistrationForm
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate,login
-from django.views.generic import TemplateView, ListView, DetailView
+
 from datetime import datetime
 from .forms import *
 from django.contrib.auth.views import LoginView
+from django.http import JsonResponse
 
 # from django.http import HttpResponseRedirect
 
@@ -21,13 +21,16 @@ classroom = Class.objects.get(class_CODE='6A')
 @login_required(login_url='/home/')
 def classroom_view(request):
   now = datetime.now()
-  classroom = Class.objects.get(class_CODE='6A')
+  class_code = request.session.get('class_code', None)
+  subject_code = request.session.get('subject_code', None)
+  classroom = Class.objects.get(class_CODE=class_code)
   students = Student.objects.filter(classroom=classroom)
   template = loader.get_template('classroom.html')
   context = {
       'class':classroom,
       'Students' : students,
       'current_time':now,
+      'subject_code': subject_code,
   }
   return HttpResponse(template.render(context,request))
 
@@ -68,7 +71,6 @@ class CustomLoginView(LoginView):
       form = StudentIDForm(request.POST)
       if form.is_valid():
           student_id = form.cleaned_data['student_id']
-
           return redirect('student_grade_view', student_ID=student_id)
       else:
           return super().post(request, *args, **kwargs)
@@ -97,48 +99,48 @@ def week_summary_view(request):
 from django.shortcuts import render, redirect
 from .models import Class
 
-# def subject_view(request, class_code):
-#     if request.method == 'POST':
-#         subject_code = request.POST.get('subject_code')
-#         # Xử lý lựa chọn của người dùng ở đây...
-#         # Ví dụ: chuyển hướng đến một trang khác với subject_code
-#         return redirect('some_view_name', subject_code=subject_code)
-#     else:
-#         class_obj = Class.objects.get(class_CODE=class_code)
-#         subjects = class_obj.subjects.all()
-#         return render(request, 'classroom.html', {'subjects': subjects})
+
 
 # def option_view(request):
-#     template = loader.get_template('option.html')
-#     classes = Class.objects.all()
-#     if request.method == 'POST':
-#         class_code = request.POST.get('class_code')
-#         class_obj = Class.objects.get(class_CODE=class_code)
-#         subjects = class_obj.subjects.all()
-#     else:
-#         subjects = None
-
-#     context = {
-#         'classes': classes,
-#         'subjects': subjects
-#     }
-#     return HttpResponse(template.render(context, request))
-
-
+#   if request.method == 'GET':
+#       class_code = request.GET.get('class_CODE')
+#       classroom = get_object_or_404(Class, class_CODE=class_code)
+#       subjects = list(classroom.subjects.values())
+#       return JsonResponse(subjects, safe=False)
+#   else:
+#       classes = Class.objects.all()
+#       return render(request, 'option.html', {'classes': classes})
 
 def option_view(request):
-    if request.method == 'POST':
-        class_code = request.POST.get('class_code')
+  classes = Class.objects.all()
+  subjects = None
+  if request.method == 'POST':
+    class_code = request.POST.get('class_code')
+    subject_code = request.POST.get('subject_code')
+    request.session['class_code'] = class_code
+    request.session['subject_code'] = subject_code
+    return redirect('Classroom')  # Redirect to the classroom view
+  elif 'class_code' in request.GET:
+    class_code = request.GET.get('class_code')
+    try:
         classroom = Class.objects.get(class_CODE=class_code)
-        subjects = classroom.subjects.all()
-    else:
-        classroom = None
-        subjects = None
-
-    classes = Class.objects.all()
-    context = {
-        'classroom': classroom,
-        'subjects': subjects,
-        'classes': classes,
-    }
-    return render(request, 'Option.html', context)
+        subjects = list(classroom.subjects.values())
+    except Class.DoesNotExist:
+        pass
+  if subjects is not None:
+    return JsonResponse(subjects, safe=False)
+  else:
+    return render(request, 'option.html', {'classes': classes})
+  # if 'class_code' in request.GET:
+  #     class_code = request.GET.get('class_code')
+  #     subject_code = request.GET.get('subject_code',None)
+  #     try:
+  #       classroom = Class.objects.get(class_CODE=class_code)
+  #       subjects = list(classroom.subjects.values())
+  #       request.session['class_code'] = class_code
+  #     except Class.DoesNotExist:
+  #       pass
+  # if subjects is not None:
+  #     return JsonResponse(subjects, safe=False)
+  # else:
+  #     return render(request, 'option.html', {'classes': classes})
